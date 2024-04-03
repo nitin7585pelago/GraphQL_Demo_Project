60b21ed05f1c6a5b9ee21495d37a234efdcf6382
nitin7585pelago <nitin.verma@pelago.co>
2024-04-03 13:02:44 +0530

    modified predefined models and created suppliers and Category

--- db/__init__.py
+++ db/__init__.py
@@ -29,7 +29,18 @@ class Product(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
-    options = relationship('Option', back_populates='product')
+    price = Column(Integer)
+    stock = Column(Integer)
+    buy = Column(Integer)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    supplier_id = Column(Integer, ForeignKey('suppliers.id'))
+    unit_id = Column(Integer, ForeignKey('units.id'))
+    
+    category = relationship('Category', back_populates='products')
+    supplier = relationship('Supplier', back_populates='products')
+    options = relationship('Option', back_populates='products')
+    units = relationship('Unit', back_populates='products')
+    
     
 class Option(Base):
     __tablename__ = 'options'
@@ -37,10 +48,35 @@ class Option(Base):
     name = Column(String)
     value = Column(String)
     product_id = Column(Integer, ForeignKey('products.id'))
+    
     product = relationship('Product', back_populates='options')
+    
     
 class Unit(Base):
     __tablename__ = 'units'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     abbreviation = Column(String)
+    product_id = Column(Integer, ForeignKey('products.id'))
+    
+    product = relationship('Product', back_populates='units')
+    
+    
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    
+    products = relationship('Product', back_populates='category')
+    
+    
+class Supplier(Base):
+    __tablename__ = 'suppliers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    contact_person = Column(String)
+    email = Column(String)
+    phone = Column(String)
+    
+    products = relationship('Product', back_populates='supplier')
